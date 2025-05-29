c7c23f847f2760164447bf1d195ba5f3ca8ba584
Bakytzhan Temirgali <ba.temirgali@gmail.com>
2025-05-29 17:11:53 +0500

    Enable google maps based on the given coordinates lat lng

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
 import axios from "axios";
 import { useState } from "react";
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
 function App() {
+
   const apiKey = import.meta.env.VITE_API_KEY;
   const [address, setAddress] = useState("");
+  const [coordinates, setCoordinates] = useState<{ lat: number; lng: number } | null>(null);
+
+  type GoogleGeocodingResponse = {
+    results: { geometry: { location: { lat: number; lng: number } } }[];
+    status: "OK" | "ZERO_RESULTS";
+  };
 
   function searchAddressHandler(event: React.FormEvent) {
     event.preventDefault();
@@ -12,31 +20,49 @@ function App() {
     if (!address.trim()) return;
 
     axios
-      .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`)
-      .then(response => {
-        console.log(response.data);
+      .get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`)
+      .then((response) => {
+        console.log(response.data)
+        if (response.data.status !== "OK") {
+          throw new Error("Could not fetch location!");
+        }
+        const coords = response.data.results[0].geometry.location;
+        setCoordinates(coords);
       })
-      .catch(error => {
+      .catch((error) => {
+        alert(error.message);
         console.error(error);
       });
   }
 
   return (
-    <>
-      <div id="map">
-        <p>Please enter an address!</p>
-      </div>
+    <APIProvider apiKey={apiKey}>
+      <div className="map-container">
+        <form onSubmit={searchAddressHandler}>
+          <input
+            type="text"
+            id="address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            placeholder="Enter address"
+          />
+          <button type="submit">Search address</button>
+        </form>
 
-      <form onSubmit={searchAddressHandler}>
-        <input
-          type="text"
-          id="address"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-        <button type="submit">Search address</button>
-      </form>
-    </>
+        {coordinates && (
+          <Map
+            center={coordinates}
+            zoom={12}
+          />
+        )}
+
+        {!coordinates && (
+          <div>
+            <p>Please enter an address!</p>
+          </div>
+        )}
+      </div>
+    </APIProvider>
   );
 }
 
